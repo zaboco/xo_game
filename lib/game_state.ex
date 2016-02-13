d6f97b79e4a36613f5a93a496810334064ba6422
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2016-02-13 21:44:02 +0200

    add GameState.check_status for :win

--- lib/game_state.ex
+++ lib/game_state.ex
@@ -3,11 +3,45 @@ defmodule GameState do
     {Board.empty, players}
   end
 
-  def check_status(game_state) do
+  def check_status board do
+    cond do
+      is_win board -> :win
+    end
+  end
+
+  defp is_win board do
+    [&rows/1, &columns/1, &diagonals/1]
+      |> Enum.flat_map(fn selector -> selector.(board) end)
+      |> Enum.any?(&same_items?/1)
+  end
+
+  defp rows matrix do
+    matrix
+  end
+
+  defp columns matrix do
+    matrix |> List.zip |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def diagonals matrix do
+    matrix
+      |> Enum.with_index
+      |> Enum.map(fn {row, i} ->
+        { Enum.at(row, i), Enum.at(row, -(i+1)) }
+      end)
+      |> Enum.unzip
+      |> Tuple.to_list
+  end
+
+  defp same_items? list do
+    case list do
+      [] -> true
+      [x | xs] -> Enum.all? xs, &(&1 == x)
+    end
   end
 
   def apply_move({board, players}, {index, sign}) do
-    new_board = board |> Board.fill_cell at: index, with: sign
+    new_board = board |> Board.fill_cell(at: index, with: sign)
     {new_board, players}
   end
 end
--- test/game_state_test.exs
+++ test/game_state_test.exs
@@ -1,8 +1,9 @@
 defmodule GameStateTest do
   use ExUnit.Case
   import :meck
+  import Board
 
-  test "starts in_progress, with empty board and with given players" do
+  test "starts with empty board and with given players" do
     players = [{:x, :human}, {:o, :computer}]
     assert GameState.initial(players) == {Board.empty, players}
   end
@@ -11,5 +12,22 @@ defmodule GameStateTest do
     expect Board, :fill_cell, [:board, [at: 2, with: :x]], :new_board
     new_state = GameState.apply_move {:board, :players}, {2, :x}
     assert new_state == {:new_board, :players}
+    unload Board
+  end
+
+  test "check_status returns :win if any row is a winner" do
+    assert GameState.check_status(~b|x x x : _ _ _ : _ _ _|) == :win
+    assert GameState.check_status(~b|_ _ _ : o o o : _ _ _|) == :win
+    assert GameState.check_status(~b|_ _ _ : o x o : x x x|) == :win
+  end
+
+  test "check_status returns :win if any column is a winner" do
+    assert GameState.check_status(~b|x _ _ : x _ _ : x _ _|) == :win
+    assert GameState.check_status(~b|_ o _ : _ o _ : _ o _|) == :win
+  end
+
+  test "check_status returns :win if any diagonal is a winner" do
+    assert GameState.check_status(~b|x _ _ : _ x _ : _ _ x|) == :win
+    assert GameState.check_status(~b|_ _ o : _ o _ : o _ _|) == :win
   end
 end
