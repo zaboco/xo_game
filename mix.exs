6a4703d312836c4a560e85f28c2d491770dbbc3a
Bogdan Zaharia <zaboco@gmail.com>
2016-02-15 09:44:00 +0200

    add Human.read_move

--- mix.exs
+++ mix.exs
@@ -13,11 +13,12 @@ defmodule XoGame.Mixfile do
   end
 
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :table_rex]]
   end
 
   defp deps do
     [
+      {:table_rex, "~> 0.8.0"},
       {:excoveralls, "~> 0.4", only: :test},
       {:meck, "~> 0.8.4", only: :test},
       {:mix_test_watch, "~> 0.2", only: :dev}
