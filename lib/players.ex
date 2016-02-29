1b14189462a5ddb9527704a94ce75c2b80b3bff2
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2016-02-29 18:45:35 +0200

    update Players to use Player protocol

--- lib/players.ex
+++ lib/players.ex
@@ -1,16 +1,36 @@
 defmodule Players do
-  def get_move board, {sign, player_type} do
-    case player_type do
-      :human -> Players.Human.read_move board, sign
-      :computer -> Players.Computer.calculate_move board, sign
+  alias Players.{Human, Computer}
+
+  def make(type_reader) do
+    [:x, :o]
+    |> Enum.map(fn sign -> make_one type_reader.(sign), sign end)
+    |> List.to_tuple
+  end
+
+  defp make_one(type, sign) do
+    case String.first(type) do
+      "h" -> %Human{sign: sign}
+      "c" -> %Computer{sign: sign}
     end
   end
 
-  def swap [current, next] do
-    [next, current]
+  def get_current_move({current, _other}, board) do
+    Player.get_move current, board
   end
 
-  def show_current [{sign, type}, _next] do
-    "#{sign}(#{type})"
+  def swap({current, next}) do
+    {next, current}
   end
+
+  def show_current({current, _other}) do
+    Player.show current
+  end
+end
+
+defprotocol Player do
+  @spec get_move(t, Matrix.t) :: number
+  def get_move(player, board)
+
+  @spec show(t) :: String.t
+  def show(player)
 end
