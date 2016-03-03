eaffb57156511bfee56f7a79dc49a615f6230b03
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2016-03-04 00:05:52 +0200

    add GameUI.Console

--- lib/game_ui.ex
+++ lib/game_ui.ex
@@ -1,9 +1,22 @@
 defmodule GameUI do
+  @doc """
+  Reads index for of the next move
+  """
   @callback read_index :: String.t
 
-  @callback read_player_type() :: String.t
+  @doc """
+  Reads player type ("human" or "computer")
+  """
+  @callback read_player_type(Board.sign) :: String.t
 
+  @doc """
+  Prints the given matrix
+  """
   @callback print_matrix([[any]]) :: nil
 
-  @callback log(atom, any) :: nil
+  @doc """
+  Logs a certain message, given by a key and an optional value
+  """
+  @typep message_type :: :wrong_index
+  @callback log(message_type, any) :: nil
 end
