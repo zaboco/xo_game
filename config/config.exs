40d8a7a816b8ccc57cf853c658102e9067194ac2
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2016-03-07 22:43:14 +0200

    rename FakeUI to GameUI.Mock and set it in config file

--- config/config.exs
+++ config/config.exs
@@ -2,3 +2,7 @@ use Mix.Config
 
 config :xo_game,
   ui: GameUI.Console
+
+if Mix.env in [:test] do
+  import_config "#{Mix.env}.exs"
+end
--- test/Players/human_test.exs
+++ test/Players/human_test.exs
@@ -1,23 +1,15 @@
 defmodule Players.HumanTest do
   use ExUnit.Case
   alias Players.Human
-  alias Doubles.FakeUI
+  alias GameUI.Mock, as: MockUI
 
   @moduletag :rewrite
 
   @empty_board Board.empty(3)
   @x_human %Human{sign: :x}
 
-  setup_all do
-    Application.put_env(:xo_game, :ui, FakeUI)
-  end
-
-  setup do
-    FakeUI.init
-    :ok
-  end
-
   test "get_move displays the board with indexes" do
+    MockUI.will_return read_index: ["1"]
     Player.get_move(@x_human, Board.empty(3))
     assert_received {:print_matrix, [
         [1, 2, 3],
@@ -27,28 +19,28 @@ defmodule Players.HumanTest do
   end
 
   test "get_move uses the given index if valid" do
-    FakeUI.will_return read_index: ["1"]
+    MockUI.will_return read_index: ["1"]
     assert @x_human |> Player.get_move(@empty_board) == {0, :x}
   end
 
   test "get_move reads the index again if not a digit" do
-    FakeUI.will_return read_index: ["a", "1"]
+    MockUI.will_return read_index: ["a", "1"]
     assert @x_human |> Player.get_move(@empty_board) == {0, :x}
   end
 
   test "get_move reads the index again if cell not empty" do
     board = @empty_board |> Board.put(0, :x)
-    FakeUI.will_return read_index: ["1", "2"]
+    MockUI.will_return read_index: ["1", "2"]
     assert @x_human |> Player.get_move(board) == {1, :x}
   end
 
   test "get_move reads the index again if out of bounds" do
-    FakeUI.will_return read_index: ["0", "4"]
+    MockUI.will_return read_index: ["0", "4"]
     assert @x_human |> Player.get_move(@empty_board) == {3, :x}
   end
 
   test "get_move logs error message if wrong index" do
-    FakeUI.will_return read_index: ["0", "4"]
+    MockUI.will_return read_index: ["0", "4"]
     @x_human |> Player.get_move(@empty_board)
     assert_received {:log, [:wrong_index, "0"]}
   end
